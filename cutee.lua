2b20b412261299e55fa06bf676933c472db7db3c
Ian Heide Godtliebsen <ian@chem.au.dk>
2018-09-11 16:06:36 +0200

    updated

--- cutee.lua
+++ cutee.lua
@@ -1,23 +1,32 @@
 function cutee()
-   homepage    ("https://github.com/IanHG/cutee")
-   .url        ("git://github.com/IanHG/cutee.git")
-   .version    ("%version%")
-   .description([[
+   -- General stuff
+   homepage   ("https://github.com/IanHG/cutee")
+   url        ("git://github.com/IanHG/cutee.git")
+   version    ("%version%")
+   description([[
       cutee : Cpp Unit TEsting Environment
 
       A minimalistic unit testing framework for C++.
    ]])
    
-   cmake(">=3.9")
-
+   -- Dependecies
+   dependson("cmake@3.9.0", "load")
+   
+   -- Build
+   build()
+      .cmake()
+      .make()
+      .makeinstall()
+   
    --.file("lol.txt", [[This is some content]])
-
-   lmod  
+   
+   -- Lmod
+   lmod()
       .help  ([[This module loads %name% v. %version%.]])
       .family("cutee")
       .group ("tools")
 
-   symbol
+   symbol()
       .add("name"   , "cutee")
       .add("version", "1.0.0")
 end
